110b13e18026f3c5c3f17664c7f1ea784a446c89
mahbub810515 <mahbub810515@gmail.com>
2025-12-08 12:41:13 +0600

    footer responsive done

--- src/App.tsx
+++ src/App.tsx
@@ -29,9 +29,9 @@ function App() {
       <section>
           <SuccesStory/>
       </section>
-      {/* <section>
+      <section>
           <Footer/>
-      </section> */}
+      </section>
      
     </>
   )
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,54 +12,54 @@ import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
     return (
-        <section className="bg-primary text-white mt-[90px]">
+        <section className="bg-primary text-white mt-10 lg:mt-[90px]">
             <Container>
-                <h1 className="pt-[30px] text-[50px] text-center font-anek font-bold text-white">ভর্তি চলছে</h1>
-                <p className="text-[20px] text-center font-anek font-regular mt-[15px] text-white">আমাদের অনলাইন অথবা অফলাইন কোর্সে এনরোল করে ফেলুন</p>
-                <div className="flex gap-[50px] ml-[340px] mt-[50px]">
-                    <h4 className="font-anek font-bold text-5 py-[9px] px-[60px] border-[1px] rounded-[10px]">সকল কোর্স দেখুন</h4>
-                    <h4 className="font-anek font-bold text-5 py-[9px] px-[60px] border-[1px] rounded-[10px]">আমাদের সেমিনারে যোগ দিন</h4>
+                <h1 className="pt-[30px] text-8 lg:text-[50px] text-center font-anek font-bold text-white">ভর্তি চলছে</h1>
+                <p className="text-4 lg:text-[20px] text-center font-anek font-regular mt-[15px] text-white">আমাদের অনলাইন অথবা অফলাইন কোর্সে এনরোল করে ফেলুন</p>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mt-9">
+                    <h4 className="font-anek font-bold text-3xl text-center py-[9px] px-5 lg:px-[60px] border rounded-[10px]">সকল কোর্স দেখুন</h4>
+                    <h4 className="font-anek font-bold text-3xl text-center py-[9px] px-5 lg:px-[60px] border rounded-[10px]">আমাদের সেমিনারে যোগ দিন</h4>
                 </div>
-                <div className="flex justify-between mt-[118px]">
+                <div className="grid grid-cols-1 lg:grid-cols-4 text-center mt-15  lg:mt-[118px]">
                     <div>
-                        <h3 className="mb-2 font-anek font-bold text-[24px]">আমাদের ঠিকানা</h3>
-                        <h4 className="flex gap-3 mb-2 font-anek font-semibold text-[24px]"><FaLocationArrow />২৮ / ২, মেহের প্লাজা, ধানমন্ডি, ঢাকা</h4>
-                        <h4 className="flex gap-3 mb-2 font-anek font-semibold text-[24px]"><IoCall />+৮৮ ০১৭১৫৫৫৭৮৯৬</h4>
-                        <h4 className="flex gap-3 mb-2 font-anek font-semibold text-[24px]"><MdEmail />info@codeduniya.com</h4>
+                        <h3 className="mb-2 font-anek font-bold text-center text-[24px]">আমাদের ঠিকানা</h3>
+                        <h5 className="flex justify-center gap-3 mb-2 font-anek font-semibold text-[16px]"><FaLocationArrow />২৮ / ২, মেহের প্লাজা,ধানমন্ডি</h5>
+                        <h5 className="flex justify-center gap-3 mb-2 font-anek font-semibold text-[16px]"><IoCall />+৮৮ ০১৭১৫৫৫৭৮৯৬</h5>
+                        <h5 className="flex justify-center gap-3 mb-2 font-anek font-semibold text-[16px]"><MdEmail />info@codeduniya.com</h5>
                     </div>
                     <div>
-                        <h3 className="mb-2 font-anek font-bold font-[24px]">গুরুত্বপূর্ণ লিংক</h3>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">আমাদের কোর্স</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">মেন্টর</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">আমাদের ব্লগ</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">প্রাইভেসি পলিসি</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">আমাদের সেমিনার</h4>
+                        <h3 className="mb-2 font-anek font-bold text-center text-[24px]">গুরুত্বপূর্ণ লিংক</h3>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">আমাদের কোর্স</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">মেন্টর</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">আমাদের ব্লগ</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">প্রাইভেসি পলিসি</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">আমাদের সেমিনার</h5>
                     </div>
                     <div>
-                        <h3 className="mb-2 font-anek font-bold font-[24px]">জনপ্রিয় কোর্সগুল</h3>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">ওয়েব ডেভেলপমেন্ট</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">গ্রাফিক্স ডিজাইন</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">অ্যাপ ডেভেলপমেন্ট</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">সাইবার সিকিউরিটি</h4>
-                        <h4 className="mb-2 font-anek font-semibold font-[24px]">ভিডিও এডিটিং</h4>
+                        <h3 className="mb-2 font-anek font-bold text-center text-[24px]">জনপ্রিয় কোর্সগুল</h3>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">ওয়েব ডেভেলপমেন্ট</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">গ্রাফিক্স ডিজাইন</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">অ্যাপ ডেভেলপমেন্ট</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">সাইবার সিকিউরিটি</h5>
+                        <h5 className="mb-2 font-anek font-semibold text-[16px]">ভিডিও এডিটিং</h5>
                     </div>
-                    <div className='w-[377px] '>
+                    <div className='max-w-[377px] '>
                         <Image imgUrl={FooterImage} alt="footer logo" />
                     </div>
                 </div>
                 <div>
-                    <h1 className="pt-[30px] text-[80px] text-center font-anek font-bold text-white">কোড দুনিয়া</h1>
-                    <h3 className="text-[40px] text-center font-anek font-regular mt-[15px] text-white">দেশের সর্ববৃহৎ আইটি ট্রেনিং প্লাটফর্ম</h3>
+                    <h1 className="pt-[30px] text-10 lg:text-[80px] text-center font-anek font-bold text-white">কোড দুনিয়া</h1>
+                    <h3 className=" text-5 lg:text-[40px] text-center font-anek font-regular mt-2 lg:mt-[15px] text-white">দেশের সর্ববৃহৎ আইটি ট্রেনিং প্লাটফর্ম</h3>
                 </div>
-                <div className='flex gap-[20px] w-[240px] ml-[500px] mt-[12px]'>
-                        <FaFacebook className='text-[50px] mr-[20px]'/>
-                        <FaInstagramSquare className='text-[50px] mr-[20px]' />
-                        <FaYoutube className='text-[50px] mr-[20px]' />
-                        <FaXTwitter className='text-[50px]' />
+                <div className=' flex  gap-5 w-60 mx-auto items-center mt-3'>
+                        <FaFacebook className='text-3xl lg:text-[40px] '/>
+                        <FaInstagramSquare className='text-3xl lg:text-[40px]' />
+                        <FaYoutube className='text-3xl lg:text-[40px]' />
+                        <FaXTwitter className='text-3xl lg:text-[40px]' />
                 </div>
-                <div className='flex justify-between mt-[130px] pb-[16px]'>
-                    <h3>২০২৪ কপিরাইট  | মাহমুদ হাসান রামীম | সর্বসত্ত্ব সংরক্ষিত</h3>
-                    <h3>প্রাইভেসি পলিসি</h3>
+                <div className='flex justify-between mt-15 lg:mt-[130px] pb-4'>
+                    <h3 className=' text-3 lg:text-xl font-regular font-anek'>২০২৪ কপিরাইট  | মাহমুদ হাসান রামীম | সর্বসত্ত্ব সংরক্ষিত</h3>
+                    <h3 className=' text-3 lg:text-xl font-regular font-anek'>প্রাইভেসি পলিসি</h3>
                 </div>
             </Container>
 
